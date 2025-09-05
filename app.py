5aff4d0f19653b4c61fc0e2b51f972e676b9df52
zahratalitha <zahratalithaputri02@gmail.com>
2025-09-05 08:10:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ def load_model_and_tokenizer():
 
     tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_DIR)
     return model, tokenizer
-model, tokenizer = load_model_and_tokenizer()
 
 id2label = {
     0: "SADNESS",
