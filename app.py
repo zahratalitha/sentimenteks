9c19589caf294f35b47e8d1c3c3054fecbd2d470
zahratalitha <zahratalithaputri02@gmail.com>
2025-09-05 07:44:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,22 +20,24 @@ TOKENIZER_DIR = "tokenizer"
 def load_model_and_tokenizer():
     model_path = hf_hub_download(repo_id=REPO_ID, filename=MODEL_FILE, repo_type="model")
     tok_zip = hf_hub_download(repo_id=REPO_ID, filename=TOKENIZER_ZIP, repo_type="model")
+
+    # Extract tokenizer
     with zipfile.ZipFile(tok_zip, "r") as zip_ref:
         zip_ref.extractall(TOKENIZER_DIR)
 
- model = keras.models.load_model(
+    # Load model dengan custom_objects
+    model = keras.models.load_model(
         model_path,
         custom_objects={
-            "TFOpLambda": tf.identity,   # untuk TFOpLambda
-            "TFBertModel": TFBertModel,  # untuk backbone BERT
+            "TFOpLambda": tf.identity,
+            "TFBertModel": TFBertModel,
         },
         compile=False,
-        safe_mode=False
+        safe_mode=False,
     )
 
     tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_DIR)
     return model, tokenizer
 
-model, tokenizer = load_model_and_tokenizer()
 
 
